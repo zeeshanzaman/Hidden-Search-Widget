b874a9c068b6dffbf0cbb1ead52fc04b74055ac1
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-09 18:06:08 +0500

    input focus

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
-const search = document.querySelector('search')
-const btn = document.querySelector('btn')
-const input = document.querySelector('input')
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
 
 
 btn.addEventListener('click', () => {
